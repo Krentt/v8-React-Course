cfe783aab9a6d4cc417ee569dd51e717ae3f0e47
Marvel <marvelphannn@gmail.com>
2023-10-27 23:38:15 +0700

    feat: course setup

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+
 const Pet = (props) => {
   return React.createElement("div", {}, [
-    React.createElement("h1", {}, porps.name),
+    React.createElement("h1", {}, props.name),
     React.createElement("h2", {}, props.animal),
     React.createElement("h2", {}, props.breed),
   ]);
@@ -24,5 +27,5 @@ const App = () => {
 };
 
 const container = document.getElementById("root");
-const root = ReactDOM.createRoot(container);
+const root = createRoot(container);
 root.render(React.createElement(App));
