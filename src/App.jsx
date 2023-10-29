7f8a8709537350f129614c85b92879dd9ad8e5a1
Marvel <marvelphannn@gmail.com>
2023-10-29 20:37:49 +0700

    feat: add useQuery

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,22 @@ import { createRoot } from "react-dom/client";
 import SearchParams from "./SearchParams";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import Details from "./Detail";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: Infinity,
+      cacheTime: Infinity
+    }
+  }
+})
 
 const App = () => {
   return (
     <BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+
       <header>
         <Link to="/">Adpot Me!</Link>
       </header>
@@ -13,6 +25,7 @@ const App = () => {
         <Route path="/details/:id" element={<Details />} />
         <Route path="/" element={<SearchParams />} />
       </Routes>
+    </QueryClientProvider>
     </BrowserRouter>
   );
 };
