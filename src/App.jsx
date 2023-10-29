f011448d2333aaa648d4bff51d2c8d09e358d81c
Marvel <marvelphannn@gmail.com>
2023-10-29 22:51:06 +0700

    feat: add modal

--- src/App.jsx
+++ src/App.jsx
@@ -8,24 +8,23 @@ const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
       staleTime: Infinity,
-      cacheTime: Infinity
-    }
-  }
-})
+      cacheTime: Infinity,
+    },
+  },
+});
 
 const App = () => {
   return (
     <BrowserRouter>
-    <QueryClientProvider client={queryClient}>
-
-      <header>
-        <Link to="/">Adpot Me!</Link>
-      </header>
-      <Routes>
-        <Route path="/details/:id" element={<Details />} />
-        <Route path="/" element={<SearchParams />} />
-      </Routes>
-    </QueryClientProvider>
+      <QueryClientProvider client={queryClient}>
+        <header>
+          <Link to="/">Adpot Me!</Link>
+        </header>
+        <Routes>
+          <Route path="/details/:id" element={<Details />} />
+          <Route path="/" element={<SearchParams />} />
+        </Routes>
+      </QueryClientProvider>
     </BrowserRouter>
   );
 };
